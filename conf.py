0876d3f2c4a1d06e77c130b3b73bdaae68067d6d
Ismail Badawi <isbadawi@gmail.com>
2013-10-01 21:06:53 -0400

    Remove argparse stuff.
    Heroku or gunicorn or whatever doesn't like it.

--- conf.py
+++ conf.py
@@ -1,14 +1,3 @@
-import argparse
 import os
 
-
-def parse_args():
-    parser = argparse.ArgumentParser(
-        description='run xpath queries on Matlab ASTs')
-    parser.add_argument(
-        '--redis_url', type=str,
-        help='URL of redis instance.')
-    return parser.parse_args()
-
-args = parse_args()
-REDIS_URL = args.redis_url or os.environ['REDISCLOUD_URL']
+REDIS_URL = os.environ.get('REDISCLOUD_URL', 'redis://localhost:6379')
