d830e9ebe103b94fd214477cb83ad824fd27e70f
Ismail Badawi <ismail@badawi.io>
2014-05-23 20:07:20 -0400

    Add dummy SECRET_KEY for development (and Travis).

--- mcbench/settings.py
+++ mcbench/settings.py
@@ -3,7 +3,7 @@ import os
 root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DB_PATH = os.path.join(root, 'mcbench.sqlite')
 DATA_ROOT = os.path.expanduser('~/mcbench-benchmarks')
-SECRET_KEY = ''
+SECRET_KEY = 'dummy'
 
 try:
     from local_settings import *
